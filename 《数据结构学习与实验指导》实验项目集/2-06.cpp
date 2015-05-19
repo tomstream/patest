64128f4af7b1d998151f858df83c5827e8ae11cf
tomstream <thomas@thomasdeMacBook-Pro.local>
2015-05-19 19:00:21 +0800

    changes for a program written in the past
    get all test cases right for 数据结构学习与实验指导》实验项目集/2-06

--- 《数据结构学习与实验指导》实验项目集/2-06.cpp
+++ 《数据结构学习与实验指导》实验项目集/2-06.cpp
@@ -1,32 +1,81 @@
+//
+//  cppprogram
+//
+//  Created by thomas on 15/5/7.
+//  Copyright (c) 2015年 thomas. All rights reserved.
+//
 
 #include <iostream>
-#include <string>
-using namespace std ;
+#include <list>
+using namespace std;
 
-
-
-int main(void)
+int main()
 {
-    int A,N;
-    cin >> A >> N;
-    string s;
-    int thisCount;
-    int carry(0);
-    char c;
-    int temp;
-    for(int i=0;i < N;i++)
+    int N,value;
+    cin>>value>>N;
+    int carry=0;
+    int bit;
+    int thisbit;
+    list<int> l;
+    
+    if(N==0)
     {
-        temp=(N-i)*A+carry;
-        thisCount = temp % 10;
-        carry = temp / 10;
-        
-        c= '0'+thisCount;
-        s= c + s;
+        cout<<0;
+        return 0;
     }
-    c='0'+carry;
-    if(carry) s= c + s;
-    if(!N) cout << '0';
-    else cout << s;
-
+    
+    for(int i=0;i<N;i++)
+    {
+        thisbit=carry+(N-i)*value;
+        bit=thisbit%10;
+        carry=thisbit/10;
+        l.push_back(bit);
+    }
+    while(carry!=0)
+    {
+        bit=carry%10;
+        carry=carry/10;
+        l.push_back(bit);
+    }
+    
+    for(auto riter=l.rbegin();riter!=l.rend();riter++)
+    {
+        cout<<*riter;
+    }
+    
     return 0;
 }
+
+//
+//#include <iostream>
+//#include <string>
+//using namespace std ;
+//
+//
+//
+//int main(void)
+//{
+//    int A,N;
+//    cin >> A >> N;
+//    string s;
+//    int thisCount;
+//    int carry(0);
+//    char c;
+//    int temp;
+//    for(int i=0;i < N;i++)
+//    {
+//        temp=(N-i)*A+carry;
+//        thisCount = temp % 10;
+//        carry = temp / 10;
+//        
+//        c= '0'+thisCount;
+//        s= c + s;
+//    }
+//    c='0'+carry;
+//    if(carry) s= c + s;
+//    if(!N) cout << '0';
+//    else cout << s;
+//
+//    return 0;
+//}
+
